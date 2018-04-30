c4de2b438aed3a93fb5c5ae3f2216068ca5eed33
EnsuingRequiem <EnsuingRequiem@github.com>
2018-04-30 00:29:24 -0500

    edits on rpi

--- Software/volume.py
+++ Software/volume.py
@@ -6,7 +6,7 @@ import subprocess
 import rotary_encoder
 import shlex
 
-awk_cmd = 'awk -F"[][]" \'/dB/ { print $4 } NR==6\' <(amixer sget Digital) | awk \'NR > 1 { exit }; 1\''
+awk_cmd = 'amixer sget Digital | awk -F"[][]" \'/dB/ { print $4 } NR==6\' | awk \'NR > 1 { exit }; 1\''
 args = shlex.split(awk_cmd)
 vol = subprocess.Popen(args)
 print vol
@@ -14,7 +14,7 @@ print vol
 def rot_callback(way):
     global vol
     pos += way
-    print ("pos={}".format(pos))
+    print ("vol={}".format(vol))
 
 pi = pigpio.pi()
 
