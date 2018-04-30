1bdd554b05b4e5396fd688981fe959eee4a1b769
EnsuingRequiem <EnsuingRequiem@Reddit>
2018-04-30 15:23:48 -0500

    different approach with popen

--- Software/volume.py
+++ Software/volume.py
@@ -7,18 +7,20 @@ import rotary_encoder
 import shlex
 from subprocess import PIPE as PIPE
 
-amix_cmd = subprocess.Popen(["amixer", "sget", "Digital"], stdout=PIPE)
-awk_cmd1 = subprocess.Popen(["awk", "-F\"[][]\" '/dB/ { print $4 } NR==6'"], stdin=amix_cmd.stdout, stdout=PIPE)
-awk_cmd2 = subprocess.Popen(["awk", "'NR > 1 { exit }; 1'"], stdin=awk_cmd1.stdout, stdout=PIPE)
+awk_cmd = subprocess.Popen(["amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $4 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'"] stdout-PIPE, shell=True)
+
+#amix_cmd = subprocess.Popen(["amixer", "sget", "Digital"], stdout=PIPE)
+#awk_cmd1 = subprocess.Popen(["awk", "-F\"[][]\" '/dB/ { print $4 } NR==6'"], stdin=amix_cmd.stdout, stdout=PIPE)
+#awk_cmd2 = subprocess.Popen(["awk", "'NR > 1 { exit }; 1'"], stdin=awk_cmd1.stdout, stdout=PIPE)
 #amix_args = shlex.split(amix_cmd)
 #awk1_args = shlex.split(awk_cmd1)
 #awk2_args = shlex.split(awk_cmd2)
 #print (amix_args)
 #print (awk1_args)
 #print (awk2_args)
-amix_cmd.stdout.close()
-awk_cmd1.stdout.close()
-vol = awk_cmd2.communicate()[0]
+#amix_cmd.stdout.close()
+#awk_cmd1.stdout.close()
+vol = awk_cmd.communicate()[0]
 #vol = subprocess.Popen(args)
 print vol
 
