7939b9774fc296e0c546b2508d3861a23db1e008
EnsuingRequiem <EnsuingRequiem@github.com>
2018-04-30 15:15:44 -0500

    pushing to edit in a gui

--- Software/volume.py
+++ Software/volume.py
@@ -5,15 +5,26 @@ import pigpio
 import subprocess
 import rotary_encoder
 import shlex
+from subprocess import PIPE as PIPE
 
-awk_cmd = 'amixer sget Digital | awk -F"[][]" \'/dB/ { print $4 } NR==6\' | awk \'NR > 1 { exit }; 1\''
-args = shlex.split(awk_cmd)
-vol = subprocess.Popen(args)
+amix_cmd = subprocess.Popen(["amixer", "sget", "Digital"], stdout=PIPE)
+awk_cmd1 = subprocess.Popen(["awk", "-F\"[][]\" '/dB/ { print $4 } NR==6'"], stdin=amix_cmd.stdout, stdout=PIPE)
+awk_cmd2 = subprocess.Popen(["awk", "'NR > 1 { exit }; 1'"], stdin=awk_cmd1.stdout, stdout=PIPE)
+#amix_args = shlex.split(amix_cmd)
+#awk1_args = shlex.split(awk_cmd1)
+#awk2_args = shlex.split(awk_cmd2)
+#print (amix_args)
+#print (awk1_args)
+#print (awk2_args)
+amix_cmd.stdout.close()
+awk_cmd1.stdout.close()
+vol = awk_cmd2.communicate()[0]
+#vol = subprocess.Popen(args)
 print vol
 
 def rot_callback(way):
     global vol
-    pos += way
+    vol += way
     print ("vol={}".format(vol))
 
 pi = pigpio.pi()
