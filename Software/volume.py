3a9c7ec340c8e4e3dd029e10c7f506f134774928
EnsuingRequiem <EnsuingRequiem@github.com>
2018-04-30 16:47:01 -0500

    prepping to move from pigpio to RPi.GPIO

--- Software/volume.py
+++ Software/volume.py
@@ -1,47 +1,36 @@
 #! /usr/bin/env python
+try:
+    import time
+    import pigpio
+    import subprocess
+    import rotary_encoder
+    import shlex
+    from subprocess import PIPE as PIPE
 
-import time
-import pigpio
-import subprocess
-import rotary_encoder
-import shlex
-from subprocess import PIPE as PIPE
+    def rot_callback(way):
+        global vol
+        #print way
+        if way > 0:
+            adj = "%sdB+" % (way)
+            #print adj
+            subprocess.Popen(["amixer", "-q", "sset", "Digital", adj])
+        if way < 0:
+            adj = "%sdB-" % (abs(way))
+	    #print adj
+            subprocess.Popen(["amixer", "-q", "sset", "Digital", adj])
+        awk_cmd = subprocess.Popen(["amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $4 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'"], stdout=PIPE, shell=True)
+        vol = awk_cmd.communicate()[0]
+        print ("vol={}".format(vol))
 
-awk_cmd = subprocess.Popen(["amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $4 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'"], stdout=PIPE, shell=True)
+    pi = pigpio.pi()
+    decoder = rotary_encoder.decoder(pi, 13, 26, rot_callback)
 
-#amix_cmd = subprocess.Popen(["amixer", "sget", "Digital"], stdout=PIPE)
-#awk_cmd1 = subprocess.Popen(["awk", "-F\"[][]\" '/dB/ { print $4 } NR==6'"], stdin=amix_cmd.stdout, stdout=PIPE)
-#awk_cmd2 = subprocess.Popen(["awk", "'NR > 1 { exit }; 1'"], stdin=awk_cmd1.stdout, stdout=PIPE)
-#amix_args = shlex.split(amix_cmd)
-#awk1_args = shlex.split(awk_cmd1)
-#awk2_args = shlex.split(awk_cmd2)
-#print (amix_args)
-#print (awk1_args)
-#print (awk2_args)
-#amix_cmd.stdout.close()
-#awk_cmd1.stdout.close()
-vol = awk_cmd.communicate()[0]
-#vol = subprocess.Popen(args)
-print vol
+except KeyboardInterrupt:
+    print "\nVolume Control Cancelled"
 
-def rot_callback(way):
-    global vol
-    if way > 0:
-        adj = "%sdB+" % (way)
-        subprocess.Popen(["amixer", "sset", "Digital", adj])
-    if way < 0:
-        adj = "%sdB-" % (way)
-        subprocess.Popen(["amixer", "sset", "Digital", adj])
-    awk_cmd = subprocess.Popen(["amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $4 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'"], stdout=PIPE, shell=True)
-    vol = awk_cmd.communicate()[0]
-    print ("vol={}".format(vol))
+except:
+    print "Some error or exception ocurred."
 
-pi = pigpio.pi()
-
-decoder = rotary_encoder.decoder(pi, 13, 26, rot_callback)
-
-time.sleep(300)
-
-decoder.cancel()
-
-pi.stop()
+finally:
+    decoder.cancel()
+    pi.stop()
