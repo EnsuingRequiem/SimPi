d219ddad0d5fecff7b2896cb3aa930a520142838
EnsuingRequiem <EnsuingRequiem@Reddit>
2018-04-30 00:23:52 -0500

    retry

--- Software/volume.py
+++ Software/volume.py
@@ -6,7 +6,7 @@ import subprocess
 import rotary_encoder
 import shlex
 
-awk_cmd = 'awk -F"[][]" '/dB/ { print $4 } NR==6' <(amixer sget Digital) | awk 'NR > 1 { exit }; 1''
+awk_cmd = 'awk -F"[][]" \'/dB/ { print $4 } NR==6\' <(amixer sget Digital) | awk \'NR > 1 { exit }; 1\''
 args = shlex.split(awk_cmd)
 vol = subprocess.Popen(args)
 print vol
