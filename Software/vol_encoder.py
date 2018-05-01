49ec2d5d21981ba5c8169cf532d6a3bf3f6b8127
EnsuingRequiem <EnsuingRequiem@Reddit>
2018-04-30 21:16:28 -0500

    added button press to the volume encoder

--- Software/vol_encoder.py
+++ Software/vol_encoder.py
@@ -1,46 +1,54 @@
 #! /usr/bin/env python
 
 #Porting the pigpio rotary_encoder sample to use RPi.GPIO
-
 import RPi.GPIO as GPIO
 
 class decoder:
-	def __init__(self, gpioA, gpioB, callback):
+    def __init__(self, gpioA, gpioB, callback, btn_pin=None, btn_callback=None):
 
-		self.lastGpio = None
-		self.gpioA = gpioA
-		self.gpioB = gpioB
-		self.callback = callback
+        self.lastGpio = None
+        self.gpioA = gpioA
+        self.gpioB = gpioB
+        self.callback = callback
 
-		self.levA = 0
-		self.levB = 0
+        self.btn_pin = btn_pin
+        self.btn_callback = btn_callback
 
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.gpioA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.setup(self.gpioB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.levA = 0
+        self.levB = 0
 
-		GPIO.add_event_detect(self.gpioA, GPIO.BOTH, self._pulse)
-		GPIO.add_event_detect(self.gpioB, GPIO.BOTH, self._pulse)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpioA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.gpioB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-	def _pulse(self, pin):
-		level = GPIO.input(pin)
-		if pin == self.gpioA:
-			self.levA = level
-		else:
-			self.levB = level
+        GPIO.add_event_detect(self.gpioA, GPIO.BOTH, self._pulse)
+        GPIO.add_event_detect(self.gpioB, GPIO.BOTH, self._pulse)
 
-		if pin != self.lastGpio:
-			self.lastGpio = pin
+        if self.btn_pin:
+            GPIO.setup(self.btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.add_event_detect(self.btn_pin, GPIO.FALLING, self._press, bouncetime=500)
 
-			if pin == self.gpioA and level == 1:
-				if self.levB == 1:
-					self.callback(1)
-			elif pin == self.gpioB and level == 1:
-				if self.levA == 1:
-					self.callback(-1)
+    def _pulse(self, pin):
+        level = GPIO.input(pin)
+        if pin == self.gpioA:
+            self.levA = level
+        else:
+            self.levB = level
 
-	def destroy(self):
-		GPIO.remove_event_detect(self.gpioA)
-		GPIO.remove_event_detect(self.gpioB)
-		GPIO.cleanup()
+        if pin != self.lastGpio:
+            self.lastGpio = pin
 
+            if pin == self.gpioA and level == 1:
+                if self.levB == 1:
+                    self.callback(1)
+            elif pin == self.gpioB and level == 1:
+                if self.levA == 1:
+                    self.callback(-1)
+
+    def destroy(self):
+        GPIO.remove_event_detect(self.gpioA)
+        GPIO.remove_event_detect(self.gpioB)
+        GPIO.cleanup()
+
+    def _press(self, pin):
+        self.btn_callback(GPIO.input(pin))
--- Software/volume.py
+++ Software/volume.py
@@ -23,8 +23,10 @@ def rot_callback(way):
     #vol = awk_cmd.communicate()[0]
     #print ("vol={}".format(vol))
 
+def btn_callback(level):
+    subprocess.Popen(["amixer", "-q", "sset", "Digital", "toggle"])
 try:
-    decoder = vol_encoder.decoder(13, 26, rot_callback)
+    decoder = vol_encoder.decoder(13, 26, rot_callback, 23, btn_callback)
     time.sleep(3000)
 except KeyboardInterrupt:
     print "\nVolume Control Cancelled"
@@ -36,3 +38,10 @@ finally:
     decoder.destroy()
     #decoder.cancel()
     #pi.stop()
+
+    #amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $6 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'
+    #above command can be passed to get the current 'sound status' (on|off = unmuted|muted)
+    #the amixer command to toggle each setting: (on|off = mute|unmute)
+    #
+    #awk_cmd = subprocess.Popen(["amixer sget Digital | awk -F\"[][]\" '/dB/ {{ print $4 }} NR==6' | awk 'NR > 1 {{ exit }}; 1'"], stdout=PIPE, shell=True)
+    #vol = awk_cmd.communicate()[0]
